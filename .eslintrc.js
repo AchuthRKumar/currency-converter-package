23f2f189c8ba454f40d45e8d6714e162a75f4afb
AchuthRKumar <achusince2002@gmail.com>
2025-10-22 23:39:05 +0530

    chore: Initialised monorepo with npm workspace

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+export default {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['@typescript-eslint', 'react', 'react-hooks', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/prop-types': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  ignorePatterns: ['node_modules/', 'dist/', 'build/'],
+};
